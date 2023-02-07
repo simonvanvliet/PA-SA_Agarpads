f7c287512bd79deea178bbe4a185a18295b663af
simonvanvliet <simonvanvliet@icloud.com>
2023-02-07 14:46:37 +0100

    updated movie script

--- movie_script_example.py
+++ movie_script_example.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 from . import utilities as utils
 
+import dask.array as da
 import re
 import importlib
 from threading import Thread
@@ -20,132 +21,119 @@ from threading import Thread
 import ffmpeg
 from skimage.morphology import skeletonize
 
+import nd2
+
+#load data
+f = nd2.ND2File(path)
+raw_data = f.to_dask()
+f.close()
+
+for p in range(raw_data.shape[1]):
+    pos = raw_data[:,p,:,:,:]
+    movie = da.map_blocks(make_movie_frame, pos)   
+    #movie should be t x RGB image
+    vidwrite(movie, 'str_with_save_path_for_pos')
+    
 
 
+def make_movie_frame(frame):
+    # add normalization / clipping here (replace min/max with quantiles)
+    #option 1
+    frame = (frame - frame.min())/(frame.max() - frame.min())
+    
+    #option 2
+    limits = np.quantile(frame, [0.05, 0.95])
 
-def results_movie(self, frames: List[int] = None) -> Any:
-    """
-    Generate movie illustrating segmentation and tracking
+    frame = (frame - limits[0])/(limits[1] - limits[0])
+    frame[frame<0] = 0 
+    frame[frame>1] = 1 
 
-    Parameters
-    ----------
-    frames : list of int or None, optional
-        Frames to generate the movie for. If None, all frames are run.
-        The default is None.
+    # RGB-ify phase
+    frame = np.repeat(frame[:, :, np.newaxis], 3, axis=-1)
+    
+    # RGB-ify GFP
+    R = np.zeros_like(frame)
+    frame = np.stack([R,frame,R],axis=-1) #test!   
 
-    Returns
-    -------
-    movie : list of 3D numpy arrays
-        List of compiled movie frames
-
-    """
-
-    # Re-read trans frames:
-    trans_frames = self.reader.getframes(
-        positions=self.position_nb,
-        channels=0,
-        frames=frames,
-        rescale=(0, 1),
-        squeeze_dimensions=False,
-        rotate=self.rotate,
+    # Add frame number text:
+    frame = cv2.putText(
+        frame,
+        text=f"frame {fnb:06d}",
+        org=(int(frame.shape[0] * 0.05), int(frame.shape[0] * 0.97)),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=1,
+        color=(1, 1, 1, 1),
+        thickness=2,
     )
-    trans_frames = trans_frames[0, :, 0]
-    if self.drift_correction:
-        trans_frames, _ = utils.driftcorr(trans_frames, drift=self.drift_values)
-    movie = []
 
-    assert isinstance(frames, list)  # FIXME: what happens if frames is None?
-    # Run through frames, compile movie:
-    for f, fnb in enumerate(frames):
+    for r, roi in enumerate(self.rois):
 
-        frame = trans_frames[f]
+        # Get chamber-specific variables:
+        colors = utils.getrandomcolors(len(roi.lineage.cells), seed=r)
+        fr = roi.label_stack[fnb]
+        assert fr is not None  # FIXME: why is it not None?
+        cells, contours = utils.getcellsinframe(fr, return_contours=True)
+        assert isinstance(cells, list)  # needed for mypy on Python < 3.8
 
-        # RGB-ify:
-        frame = np.repeat(frame[:, :, np.newaxis], 3, axis=-1)
+        if roi.box is None:
+            xtl, ytl = (0, 0)
+        else:
+            xtl, ytl = (roi.box["xtl"], roi.box["ytl"])
 
-        # Add frame number text:
-        frame = cv2.putText(
-            frame,
-            text=f"frame {fnb:06d}",
-            org=(int(frame.shape[0] * 0.05), int(frame.shape[0] * 0.97)),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=1,
-            color=(1, 1, 1, 1),
-            thickness=2,
-        )
+        # Run through cells in labelled frame:
+        for c, cell in enumerate(cells):
 
-        for r, roi in enumerate(self.rois):
+            # Draw contours:
+            frame = cv2.drawContours(
+                frame,
+                contours,
+                c,
+                color=colors[cell],
+                thickness=1,
+                offset=(xtl, ytl),
+            )
 
-            # Get chamber-specific variables:
-            colors = utils.getrandomcolors(len(roi.lineage.cells), seed=r)
-            fr = roi.label_stack[fnb]
-            assert fr is not None  # FIXME: why is it not None?
-            cells, contours = utils.getcellsinframe(fr, return_contours=True)
-            assert isinstance(cells, list)  # needed for mypy on Python < 3.8
+            # Draw poles:
+            oldpole = roi.lineage.getvalue(cell, fnb, "old_pole")
+            assert isinstance(oldpole, np.ndarray)  # for mypy
+            frame = cv2.drawMarker(
+                frame,
+                (oldpole[1] + xtl, oldpole[0] + ytl),
+                color=colors[cell],
+                markerType=cv2.MARKER_TILTED_CROSS,
+                markerSize=3,
+                thickness=1,
+            )
 
-            if roi.box is None:
-                xtl, ytl = (0, 0)
-            else:
-                xtl, ytl = (roi.box["xtl"], roi.box["ytl"])
+            daughter = roi.lineage.getvalue(cell, fnb, "daughters")
+            bornago = roi.lineage.cells[cell]["frames"].index(fnb)
+            mother = roi.lineage.cells[cell]["mother"]
 
-            # Run through cells in labelled frame:
-            for c, cell in enumerate(cells):
-
-                # Draw contours:
-                frame = cv2.drawContours(
-                    frame,
-                    contours,
-                    c,
-                    color=colors[cell],
-                    thickness=1,
-                    offset=(xtl, ytl),
-                )
-
-                # Draw poles:
-                oldpole = roi.lineage.getvalue(cell, fnb, "old_pole")
-                assert isinstance(oldpole, np.ndarray)  # for mypy
+            if daughter is None and (bornago > 0 or mother is None):
+                newpole = roi.lineage.getvalue(cell, fnb, "new_pole")
                 frame = cv2.drawMarker(
                     frame,
-                    (oldpole[1] + xtl, oldpole[0] + ytl),
-                    color=colors[cell],
+                    (newpole[1] + xtl, newpole[0] + ytl),
+                    color=[1, 1, 1],
                     markerType=cv2.MARKER_TILTED_CROSS,
                     markerSize=3,
                     thickness=1,
                 )
 
-                daughter = roi.lineage.getvalue(cell, fnb, "daughters")
-                bornago = roi.lineage.cells[cell]["frames"].index(fnb)
-                mother = roi.lineage.cells[cell]["mother"]
+            # Plot division arrow:
+            if daughter is not None:
 
-                if daughter is None and (bornago > 0 or mother is None):
-                    newpole = roi.lineage.getvalue(cell, fnb, "new_pole")
-                    frame = cv2.drawMarker(
-                        frame,
-                        (newpole[1] + xtl, newpole[0] + ytl),
-                        color=[1, 1, 1],
-                        markerType=cv2.MARKER_TILTED_CROSS,
-                        markerSize=3,
-                        thickness=1,
-                    )
-
-                # Plot division arrow:
-                if daughter is not None:
-
-                    newpole = roi.lineage.getvalue(cell, fnb, "new_pole")
-                    daupole = roi.lineage.getvalue(daughter, fnb, "new_pole")
-                    # Plot arrow:
-                    frame = cv2.arrowedLine(
-                        frame,
-                        (newpole[1] + xtl, newpole[0] + ytl),
-                        (daupole[1] + xtl, daupole[0] + ytl),
-                        color=(1, 1, 1),
-                        thickness=1,
-                    )
-
-        # Add to movie array:
-        movie += [(frame * 255).astype(np.uint8)]
-
-    return movie
+                newpole = roi.lineage.getvalue(cell, fnb, "new_pole")
+                daupole = roi.lineage.getvalue(daughter, fnb, "new_pole")
+                # Plot arrow:
+                frame = cv2.arrowedLine(
+                    frame,
+                    (newpole[1] + xtl, newpole[0] + ytl),
+                    (daupole[1] + xtl, daupole[0] + ytl),
+                    color=(1, 1, 1),
+                    thickness=1,
+                )
+    return (frame * 255).astype(np.uint8)
 
 
 
