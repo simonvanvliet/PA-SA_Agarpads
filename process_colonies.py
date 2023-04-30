1fca63f4926cbe90b7a27192249d3583e82bfc14
simonvanvliet <simonvanvliet@icloud.com>
2023-04-30 16:58:13 +0200

    Speed up colony processing

--- process_colonies.py
+++ process_colonies.py
@@ -5,12 +5,140 @@ import dask_image.ndmorph as damorph
 import skimage.filters as filters
 from skimage.measure import label, regionprops_table
 from skimage import morphology
+import h5py
+import os
 
 #dask cash
 from dask.cache import Cache
 cache = Cache(2e9)  # Leverage two gigabytes of memory
 cache.register()    # Turn cache on globally
 
+def process_pos(pos, settings, store_2_disk = True, clean_disk = True):
+    '''
+    segment & track colonies and extract properties for a single position
+    
+    Parameters
+    
+    pos: str
+        position file name
+    settings: dict
+        dictionary of settings
+    store_2_disk: bool
+        store label images to disk [speeds up processing]
+    clean_disk: bool
+        delete label images from disk after processing    
+        
+    returns:
+         pandas dataframe with colony properties             
+    '''
+    #get psotion number
+    file_name = pos.split('-images')[0]
+    pos_idx = int(file_name.split('_p')[-1])
+    
+    print(f"Segementing & processing position {pos_idx}")
+    
+    exp_name = settings['exp_name']
+    
+    #construct file names
+    file_name_im = f"{exp_name}_reg_p{pos_idx:03d}.h5"
+    file_name_seg = f"{exp_name}_reg_p{pos_idx:03d}-images_Probabilities.h5"
+
+    #load registered images
+    reg_im_file = h5py.File(settings['path_reg_im']/file_name_im, 'r') #open 
+    chunk_size = (1, *reg_im_file['images'].shape[-3:])
+    reg_im = da.from_array(reg_im_file['images'], chunks=chunk_size)
+
+    #load segmented images
+    seg_im_file = h5py.File(settings['path_seg_im']/file_name_seg, 'r') #open 
+    chunk_size = (1, 1,*reg_im_file['images'].shape[-2:])
+    seg_prob = da.from_array(seg_im_file['exported_data'], chunks=chunk_size)
+
+    #crop to max frame
+    pdata = settings['pos_metadata']
+    max_frm = int(pdata.loc[f"pos{pos_idx:03d}","max_frame"]) if f"pos{pos_idx:03d}" in pdata.index else reg_im.shape[0]
+    reg_im = reg_im[:max_frm]       
+    seg_prob = seg_prob[:max_frm]       
+    condition = pdata.loc[f"pos{pos_idx:03d}","condition"] if f"pos{pos_idx:03d}" in pdata.index else "NA"
+    
+    #convert to labels
+    SA1_labels = process_seg(seg_prob[:,settings['idx_SA1'],:,:], 
+                                sigma = settings['sigma'],
+                                threshold = settings['threshold_SA'],
+                                closing_radius = settings['closing_radius_SA'],
+                                min_cell_area = settings['min_cell_area_SA'],
+                                max_hole_area = settings['max_hole_area_SA'])
+                                
+    SA2_labels = process_seg(seg_prob[:,settings['idx_SA2'],:,:], 
+                            sigma = settings['sigma'],
+                            threshold = settings['threshold_SA'],
+                            closing_radius = settings['closing_radius_SA'],
+                            min_cell_area = settings['min_cell_area_SA'],
+                            max_hole_area = settings['max_hole_area_SA'])                       
+
+    PA_labels = process_seg(seg_prob[:,settings['idx_PA'],:,:], 
+                            sigma = settings['sigma'],
+                            threshold = settings['threshold_PA'],
+                            closing_radius = settings['closing_radius_PA'],
+                            min_cell_area = settings['min_cell_area_PA'],
+                            max_hole_area = settings['max_hole_area_PA'])     
+  
+    if store_2_disk:
+        #storing label images on local disk will speed up processing a lot (>> then factor 10)
+        #set filenames
+        file_name_lab_SA1 = f"{exp_name}_reg_p{pos_idx:03d}-temp_labels_SA1.h5"
+        file_name_lab_SA2 = f"{exp_name}_reg_p{pos_idx:03d}-temp_labels_SA2.h5"
+        file_name_lab_PA = f"{exp_name}_reg_p{pos_idx:03d}-temp_labels_PA.h5"
+
+        #write to disk
+        SA1_labels.to_hdf5(settings['temp_path'] / file_name_lab_SA1, '/labels')
+        SA2_labels.to_hdf5(settings['temp_path'] / file_name_lab_SA2, '/labels')
+        PA_labels.to_hdf5(settings['temp_path'] / file_name_lab_PA, '/labels')
+        
+        #load from disk as dask arrays
+        SA1_file = h5py.File(settings['temp_path']/file_name_lab_SA1, 'r') #open 
+        chunk_size = (1,*SA1_file['labels'].shape[-2:])
+        SA1_labels = da.from_array(SA1_file['labels'], chunks=chunk_size)
+        
+        SA2_file = h5py.File(settings['temp_path']/file_name_lab_SA2, 'r') #open 
+        chunk_size = (1,*SA2_file['labels'].shape[-2:])
+        SA2_labels = da.from_array(SA2_file['labels'], chunks=chunk_size)
+        
+        PA_file = h5py.File(settings['temp_path']/file_name_lab_PA, 'r') #open 
+        chunk_size = (1,*PA_file['labels'].shape[-2:])
+        PA_labels = da.from_array(PA_file['labels'], chunks=chunk_size)
+    
+
+    #track colonies and extract properties
+    df_SA1 = track_extract_prop(SA1_labels, settings['prop_list'], 
+                                metadata = {'pos': pos_idx, 'strain':'SA1','condition':condition})
+    
+    df_SA2 = track_extract_prop(SA2_labels, settings['prop_list'], 
+                                metadata = {'pos': pos_idx, 'strain':'SA2','condition':condition})
+    
+    df_PA = track_extract_prop(PA_labels, settings['prop_list'], 
+                                metadata = {'pos': pos_idx, 'strain':'PA','condition':condition})
+    
+    #combine dataframes
+    df = pd.concat([df_SA1, df_SA2, df_PA]).reset_index(drop=True)
+    
+    #add spatial properties
+    df = add_centrod_distance(df)
+    #if settings['calc_edge_dist']:
+    df = add_edge2edge_distance(df,PA_labels,SA1_labels,SA2_labels)
+    
+    csv_dir_pos = settings['path_data_files'] / settings['exp_name']
+    csv_name = csv_dir_pos / f"{exp_name}_pos{pos_idx:03d}.csv"
+    df.to_csv(csv_name)
+    
+    if store_2_disk and clean_disk:
+        #remove temp files        
+        os.remove(settings['temp_path'] / file_name_lab_SA1)
+        os.remove(settings['temp_path'] / file_name_lab_SA2)
+        os.remove(settings['temp_path'] / file_name_lab_PA)
+
+    return df
+    
+        
 
 def process_seg(seg_prob, sigma=1, threshold=0.5, closing_radius=5, min_cell_area=20, max_hole_area=100, channel_axis=1, output_intermediate=False):
     """convert probability map from Ilastik into semantic segmentation
@@ -240,7 +368,7 @@ def calc_min_dist(target, source):
     
     Returns
     -------
-    numpy.ndarray
+    min_distance: numpy.ndarray
         distance to closest source point for each target colony
     
     """
@@ -264,12 +392,16 @@ def calc_min_dist(target, source):
     else:
         raise ValueError('source must be tuple or numpy.ndarray')
 
-    #distance from target to source
-    dx = np.atleast_2d(target_x).T - np.atleast_2d(source_x) #row is target col is source
-    dy = np.atleast_2d(target_y).T - np.atleast_2d(source_y) #row is target col is source
-    ds = np.sqrt(dx**2 + dy**2)
+    if target_x.size > 0 and source_x.size > 0: 
+        #distance from target to source
+        dx = np.atleast_2d(target_x).T - np.atleast_2d(source_x) #row is target col is source
+        dy = np.atleast_2d(target_y).T - np.atleast_2d(source_y) #row is target col is source
+        ds = np.sqrt(dx**2 + dy**2)
+        min_distance = np.min(ds, axis=1)
+    else:
+        min_distance = np.array([np.nan])
 
-    return np.min(ds, axis=1) 
+    return min_distance
         
 
 def add_centrod_distance(df):
@@ -327,10 +459,10 @@ def add_edge2edge_distance(df,PA_labels,SA1_labels,SA2_labels):
     df = df.copy()
     df['min_dist_PA_edge2edge'] = np.nan
     
+    #get image size
+    imH,imW = PA_labels.shape[-2:]
+    
     for frm in df['frame'].unique():
-
-        #get pixels of PA colony
-        x_PA, y_PA = np.nonzero(PA_labels[frm,:])
 
         for i in df.index[(df['frame']==frm)]: #loop through colonies in frame
             if df.loc[i, 'strain'] == 'PA': #skip Pseudomonas aeruginosa
@@ -338,9 +470,20 @@ def add_edge2edge_distance(df,PA_labels,SA1_labels,SA2_labels):
             if df.loc[i, 'colony_id'] == -1: #skip untracked colonies
                 continue
             
+            #select correct SA layer
+            SA_im = SA1_labels[frm,:] if df.loc[i, 'strain'] == 'SA1' else SA2_labels[frm,:] 
+            
+            #create crop box centered on colony with size 2 x distance to closest PA colony (we know that closest Pa colony is within this distance)
+            xmin = max(0, int(df.loc[i,'centroid-1'] - df.loc[i, 'min_dist_PA_centroid']))
+            xmax = min(imW, int(df.loc[i,'centroid-1'] + df.loc[i, 'min_dist_PA_centroid']))
+            ymin = max(0, int(df.loc[i,'centroid-0'] - df.loc[i, 'min_dist_PA_centroid']))
+            ymax = min(imH, int(df.loc[i,'centroid-0'] + df.loc[i, 'min_dist_PA_centroid']))
+
             #get pixels of target colony
-            target_im = SA1_labels[frm,:] if df.loc[i, 'strain'] == 'SA1' else SA2_labels[frm,:] 
-            x_SA, y_SA = np.nonzero(target_im == df.loc[i, 'label'])
+            x_SA, y_SA = np.nonzero((SA_im[ymin:ymax,xmin:xmax] == df.loc[i, 'label']).compute())
+
+            #get pixels of PA colony
+            x_PA, y_PA = np.nonzero((PA_labels[frm,ymin:ymax,xmin:xmax]).compute())
             
             #calculate distance to closest PA colony
             dist = calc_min_dist((x_SA, y_SA), (x_PA, y_PA))            
